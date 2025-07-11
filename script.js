ad19f6a3829637225e0febd2e89b1ef39bc42ff4
Kevin7302 <56976367+Kevin7302@users.noreply.github.com>
2025-07-11 13:41:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ const markdownToHTML = (text) => {
     return converter.makeHtml(text)
 }
 
-// AIzaSyBAzOZJgqXHGSpQxSZIg1ViPbW6aJGiP8Q = chave API
 const requestAI = async (question, game, apiKey) => {
     const model = "gemini-2.5-flash"
     const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
