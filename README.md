0670561b53659d443114948c0c773e555ffcf15f
Kevin Wallace Cidrão Santiago <01900600692@ibama.gov.br>
2025-07-11 11:31:38 -0300

    Primeiro commit do projeto Assistende IA

---
+++ README.md
@@ -0,0 +1,140 @@
+# HTML - Linguagem de marcação de Texto
+
+- HyperText
+  - Textos puros, livros, revistas, jornais;
+  - Links: acessar outros documentos;
+  - Imagens, cídeos, áudios.
+- Markup
+  - Marcação
+  - Tags: <a> Link </a>
+  - Atributos: <a href="https://rocketseat.com.br"></a>
+  - Globais: id, class, ...
+- Language
+  - Linguagem;
+  - Sintaxe: maneira de escrever
+
+# HTTP
+
+- HyperText
+- Transfer
+  - Transferência
+- Protocol
+  - Protocolo
+  - Conjunto de regras
+- Methods HTTP: GET(Pegar informação), POST(Enviar Informação), PUT (Atualizar informação), DELETE(Deletar informação)
+- Headers (cabeçalhos): Instruções/Informações extras para cada chamada
+
+# URL
+
+- Uniform
+  - Uniforme
+- Resource
+  - Recurso
+- Locator
+  - Localizador
+
+Encontrar um recurso (html, css, js, pdf, png, jpg, mp4, mp3, ...)
+Endereço
+
+# IP
+
+- Internet
+  - Rede Mundial de computadores
+- Protocol
+  - Conjunto de Regras
+- Endereço do computador
+rocketseat.com.br (domínio)
+123.32.1.23 (IP)
+
+# DNS
+
+- Domain
+  - Domínio
+- Name
+  - Nome
+- Server
+  - Servidor
+
+  # CSS - Linguagem de Estilo
+
+- Cascading
+   - Cascata
+   - Regras de escritas
+   - Hierarquia
+   - Especificidade
+- Style
+  - Estilo
+- Sheet
+  - Folha
+
+
+Estamos falando de um arquivo que terá na sua sintaxe, declarações, propriedades e valores.
+A partir disso, o HTML é impactado visualmente.
+
+- declaração
+- seletor
+- propriedade e valor
+
+# JavaScript (JS)
+
+- Linguagem de programação
+- Browsers
+- Input -> Process -> Output
+- Variáveis
+- Function
+  - Agrupamento e reutilização de código
+  - Sequência lógica
+  - Saída
+
+- Gemini (IA): esperar uma resposta
+- Mexer no meu navegador
+- Estrutura de dados
+- Estrutura de decisão
+- Algoritmo: sequência de passos, de maneira ordenada, a fim de chegar à alguma conclusão (limitada). Um passo a passo bem detalhadoe lógico.
+- Lógica (Pensamento computacional)
+  - Abstração
+  - Decomposição
+  - Algoritmo
+  - Reconhecimento de padrão
+- Tipos de dados
+  - Number (números)
+  - String (texto) (" ", ' ', ` `)
+  - Boolean (true | false)
+  - Objects
+
+# DOM
+
+- Document
+ - Documento
+- Object
+  - Objeto
+- Model
+  - Modelo
+
+# API
+
+- Aplication
+- Programming
+- Interface
+- JSON: JavaScript Object Notation
+
+# CDN
+
+- Content
+- Delivery
+- Network
+
+# LLM
+- Large
+- Language
+- Model
+- Gemini, OpenAI (chatGPT) Anthropic (Claude)...
+
+# Agentes AI
+- Tools (Ferramentar): Permitem o uso de código, ou apps, ou qualquer informação extra
+- Melhor contexto
+
+# Engenharia de Prompt
+- One shot: uma única pergunta sem muito contexto
+- Few shot: é apresentado exemplos do que se espera
+- Chain of Thought: Cadei de pensamento para a IA responder gradativamente conforme a instrução.
---
+++ script.js
@@ -0,0 +1,184 @@
+const apiKeyInput = document.getElementById('apiKey');
+const gameSelect = document.getElementById('gameSelect');
+const questionInput = document.getElementById('questionInput');
+const askButton = document.getElementById('askButton');
+const aiResponse = document.getElementById('aiResponse');
+const form = document.getElementById('form');
+
+const markdownToHTML = (text) => {
+    console.log('showdown', showdown) // <- Deve mostrar o objeto com método .Converter
+    const converter = new showdown.Converter()
+    return converter.makeHtml(text)
+}
+
+// AIzaSyBAzOZJgqXHGSpQxSZIg1ViPbW6aJGiP8Q = chave API
+const requestAI = async (question, game, apiKey) => {
+    const model = "gemini-2.5-flash"
+    const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+    const requestLol = `
+        ## Especialidade
+        Você é um especialista assistente de meta para o jogo ${game}
+
+        ## Tarefa
+        Você deve responder as perguntas do usuário com base no seu conhecimento do jogo, estratégias, builds e dicas
+
+        ##Regras
+        - Se você não sabe a resposta, responda com 'Não sei' e não tente inventar uma resposta.
+        - Se a pergunta não está relacionada ao ${game}, responda com 'Essa pergunta não está relacionada ao ${game}'
+        - Considere a data atual ${new Date().toLocaleDateString()}
+        - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coerente.
+        - Nunca responda itens que você não tenha certeza de que existe no patch atual
+        - Se a ${question} não for identificada corretamente, não tente adivinhar a pergunta do usuário. Apenas responda que não entendeu a pergunta.
+        - Identifique a linguagem do usuário e dê sua resposta de acordo com a linguagem que ele escrever dentro do campo de pergunta.
+
+        ## Resposta
+        - Economize na resposta, seja direto e responda no máximo 500 caracteres.
+        - Responda em markdown
+        - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo.
+
+        ## Exemplo de resposta
+        Pergunta do usuário: Melhor build rengar jungle
+        Resposta: A builda mais atual é: \n\n **Itens:**\n\n coloque os itens aqui.\n\n**Runas:**\n\n exemplo de runas\n\n
+
+        ---
+        Aqui está a pergunta do usuário: ${question}
+    `
+    const requestValorant = `
+        ## Especialidade
+        Você é um especialista assistente de meta para o jogo ${game}
+
+        ## Tarefa
+        Você deve responder as perguntas do usuário com base no seu conhecimento do jogo, estratégias, builds e dicas
+
+        ## Regras
+        - Se você não sabe a resposta, responda com 'Não sei' e não tente inventar uma resposta.
+        - Se a pergunta não está relacionada ao ${game}, responda com 'Essa pergunta não está relacionada ao ${game}'
+        - Considere a data atual ${new Date().toLocaleDateString()}
+        - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coerente.
+        - Nunca responda algo que você não tenha certeza de que existe no patch atual
+        - Se a ${question} não for identificada corretamente, não tente adivinhar a pergunta do usuário. Apenas responda que não entendeu a pergunta.
+        - Identifique a linguagem do usuário e dê sua resposta de acordo com a linguagem que ele escrever dentro do campo de pergunta.
+
+        ## Resposta
+        - Economize na resposta, seja direto e responda no máximo 500 caracteres.
+        - Responda em markdown
+        - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo.
+
+        ## Exemplo de Resposta
+        Pergunta do usuário: Me fale sobre estratégias para aplicar durante o jogo e quais campeões são mais recomendados para essa estratégia.
+        Resposta: A estratégia é: \n\n O melhor campeão para utilizar essa estratégia é: \n\n
+
+        ---
+        Aqui está a pergunta do usuário: ${question}
+
+    `
+    const requestCsgo = `
+        ## Especialidade
+        Você é um especialista assistente de meta para o jogo ${game}
+
+        ## Tarefa
+        Você deve responder as perguntas do usuário com base no seu conhecimento do jogo, estratégias, builds e dicas
+
+        ## Regras
+        - Se você não sabe a resposta, responda com 'Não sei' e não tente inventar uma resposta.
+        - Se a pergunta não está relacionada ao ${game}, responda com 'Essa pergunta não está relacionada ao ${game}'
+        - Considere a data atual ${new Date().toLocaleDateString()}
+        - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coerente.
+        - Nunca responda algo que você não tenha certeza de que existe no patch atual
+        - Se a ${question} não for identificada corretamente, não tente adivinhar a pergunta do usuário. Apenas responda que não entendeu a pergunta.
+        - Identifique a linguagem do usuário e dê sua resposta de acordo com a linguagem que ele escrever dentro do campo de pergunta.
+
+        ## Resposta
+        - Economize na resposta, seja direto e responda no máximo 500 caracteres.
+        - Responda em markdown
+        - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo.
+
+        ## Exemplo de resposta
+        Pergunta do usuário: Fale sobre como é a jogabilidade do CS:GO e como colocar em prática a economia na compra de armas ou outros artefatos. Me fale também quais são as armas mais clássicas usadas durante o jogo.
+        Resposta: A jogabilidade do CS:GO é: \n\n Você pode economizar das seguintes formas: \n\n As armas mais clássicas e recomendadas são: \n\n
+
+        ---
+        Aqui está a pergunta do usuário: ${question}
+    `
+
+    let request = ''
+
+    switch (game) {
+        case 'lol':
+            request = requestLol
+            break;
+
+        case 'valorant':
+            request = requestValorant
+            break;
+
+        case 'csgo':
+            request = requestCsgo
+            break;
+    }
+
+    const contents = [{
+        role: "user",
+        parts: [{
+            text: request
+        }]
+    }]
+
+    const tools = [{
+        google_search: {}
+    }]
+
+    //Chamada API
+    const response = await fetch(geminiURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            contents,
+            tools
+        })
+    })
+
+    const data = await response.json()
+    return data.candidates[0].content.parts[0].text
+
+
+}
+
+const sendForm = async (event) => {
+    event.preventDefault()
+    const apiKey = apiKeyInput.value
+    const game = gameSelect.value
+    const question = questionInput.value
+
+    console.log({ apiKey, game, question })
+
+    if (apiKey == '' || game == '' || question == '') {
+        alert('Por favor, preencha todos os campos')
+        return
+    }
+
+    askButton.disabled = true;
+    askButton.textContent = 'Perguntando...'
+    askButton.classList.add('loading')
+
+    try {
+        // Perguntar para a IA
+        const text = await requestAI(question, game, apiKey)
+        aiResponse.querySelector('.response-content').innerHTML = markdownToHTML(text)
+        aiResponse.classList.remove('hidden')
+    }
+
+    catch (error) {
+        console.log('Erro: ', error)
+    }
+
+    finally {
+        askButton.disabled = false
+        askButton.textContent = "Perguntar"
+        askButton.classList.remove('loading')
+    }
+
+}
+form.addEventListener('submit', sendForm)
